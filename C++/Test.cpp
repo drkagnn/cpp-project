c3b88d44415b2db5ddd55c1686feb9ffbf908f61
drkagn <3zalternate@gmail.com>
2026-04-01 20:36:25 +0700

    sql

--- C++/Test.cpp
+++ C++/Test.cpp
@@ -1,7 +1,8 @@
-        #include <iostream>
+#include <iostream>
 #include <vector>
 #include <conio.h>
 #include <cstdlib>
+#include "sqlite3.h"
 
 void logoname()
 {
@@ -21,49 +22,103 @@ void logoname()
 std::string logpage(const std::string& input);
 
 
+static int callback(void* data, int argc, char** argv, char** colName) {
+    bool* found = (bool*)data;
+    *found = true;
+    return 0;
+}
 
 
+
+    
 //place holders for the functions
 void signin()
 {
+    sqlite3* db;
+
+    // OPEN DB
+    sqlite3_open("Account.db", &db);
+
+    // CREATE TABLE
+    std::string create_sql =
+        "CREATE TABLE IF NOT EXISTS users("
+        "name TEXT, password TEXT);";
+
+    sqlite3_exec(db, create_sql.c_str(), 0, 0, 0);
+
+    // INPUT
     std::string name;
     std::string pwd;
+
     std::cout << "Signin \nUsername: ";
     std::cin >> name;
     std::cout << "\nPassword: ";
     std::cin >> pwd;
-    
-    //write into data
+
+    // INSERT DATA
+    std::string insert_sql =
+        "INSERT INTO users (name, password) VALUES ('" + name + "', '" + pwd + "');";
+
+    sqlite3_exec(db, insert_sql.c_str(), 0, 0, 0);
+
+    // CLOSE DB
+    sqlite3_close(db);
+
     system("cls");
     logpage("");
 }
     
 std::string login()
-{   
-    
-    std::string name;
-    std::string pwd;
+{
+    sqlite3* db;
+
+    //open db
+    sqlite3_open("Account.db", &db);
+
+    std::string name, pwd;
     std::cout << "login \nUsername: ";
     std::cin >> name;
     std::cout << "\nPassword: ";
     std::cin >> pwd;
-    system("cls");
-    return name;    
-}
 
+    bool found = false;
+
+    std::string sql =
+        "SELECT * FROM users WHERE name='" + name +
+        "' AND password='" + pwd + "';";
+
+    sqlite3_exec(db, sql.c_str(), callback, &found, 0);
+
+    
+    sqlite3_close(db);
+
+    system("cls");
+
+    if (found) {
+        return name;
+    } else {
+        std::cout << "Login failed!\n";
+        return logpage("");
+    }
+}
 std::string logpage(const std::string& input)
 {   
     std::string username;
     char pick;
-    std::cout << "1. Sign in \n2. Log in\nType 1/2: ";
+    std::cout << "1. Sign in \n2. Log in\n3. Exit\nType 1/2/3: ";
     std::cin >> pick;
 
     switch (pick)
     {
     case '1':
         signin();
+        return 0;
+        break;
     case '2':
         return login();
+    case '3':
+        exit(0);
+        break;
     default:
         return "Invalid input";
     }
@@ -71,6 +126,8 @@ std::string logpage(const std::string& input)
 
 int main()
 {   
+    
+
     std::string username;
     logoname();
     username = logpage("");
