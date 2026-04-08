a50db1dc6935a57794c6e781f157b529a3d456f0
Drkagn <3zalternate@gmail.com>
2026-04-08 17:40:28 +0700

    update

--- C++/Test.cpp
+++ C++/Test.cpp
@@ -5,8 +5,10 @@
 #include "sqlite3.h"
 
 
-void logoname()
-{
+/* to compile use gcc -c sqlite3.c  this will make sqlite3.o */
+/* to compile use g++ main.cpp sqlite3.o -o app.exe */
+
+void logoname() {
     std::cout << "======================================================== \n\n";
     std::cout << "######    ######    ##   ##    ###      ######   ##   ## \n";
     std::cout << "##   ##   ##   ##   ##  ##    ## ##    ##        ###  ## \n";
@@ -17,24 +19,18 @@ void logoname()
     _getch();
     system("cls");
 }
-
-
 //forward declaration
 std::string logpage(const std::string& input);
 
-
+//SQL
 static int callback(void* data, int argc, char** argv, char** colName) {
     bool* found = (bool*)data;
     *found = true;
     return 0;
 }
 
-
-
-    
 //place holders for the functions
-void signin()
-{
+void signin() {
     sqlite3* db;
 
     // OPEN DB
@@ -68,9 +64,7 @@ void signin()
     system("cls");
     logpage("");
 }
-    
-std::string login()
-{
+std::string login() {
     sqlite3* db;
 
     //open db
@@ -102,22 +96,23 @@ std::string login()
         return logpage("");
     }
 }
-std::string logpage(const std::string& input)
-{   
+std::string logpage(const std::string& input) {   
     std::string username;
     char pick;
     std::cout << "1. Sign in \n2. Log in\n3. Exit\nType 1/2/3: ";
     std::cin >> pick;
 
-    switch (pick)
-    {
+    std::string x;
+    switch (pick) {
     case '1':
         signin();
         return 0;
         break;
     case '2':
-        return login();
+        x = login();
+        return x;
     case '3':
+        system("cls");
         exit(0);
         break;
     default:
@@ -127,12 +122,42 @@ std::string logpage(const std::string& input)
 
 int main()
 {   
-    
+    char pick;
 
     std::string username;
     logoname();
     username = logpage("");
-    std::cout << "Welcome " << username << "!";  
+    std::cout << "Welcome " << username << "!\n"; 
+    
+    std::cout << "What would you like to do?";
+    std::cout << "\n1. Statistic Calculator\n2. Exit\n(1/2): ";
+
+
+    bool run = true;
+    while(run == true) {
+        std::cin >> pick;
+        switch (pick) {
+        case '1':
+            system("cls");
+            std::cout << "Not available";
+            std::cout << "\n1. Statistic Calculator\n2. Exit\n(1/2): ";
+            break;
+        
+        case '2':
+            run = false;
+            system("cls");
+            std::cout << "Program closed \n";
+            break; 
+
+        default:
+            system("cls");
+            std::cout << "Error";
+            std::cout << "\n1. Statistic Calculator\n2. Exit\n(1/2): ";
+            break;
+        }
+    }
+
+
     return 0;
 
 }
--- C++/Untitled-1.cpp
+++ C++/Untitled-1.cpp
@@ -1,24 +1,110 @@
-#include <cstdlib>
 #include <iostream>
-#include <windows.h>
+#include <vector>
+#include <conio.h>
+#include <limits>
+#include <map>
+
+
+//g++ untitled-1.cpp -o main
+int mean(std::vector<int> data){
+    int len = data.size();
+    int sum;   
+    for(int x : data){  
+        sum = sum + x;
+    }
+    int mean = sum / len;
+
+    return mean
+    
+}
+
+int mode(std::vector<int> data){
+    std::map<int, int> freq;
+
+    // count frequency
+    for (int x : data) {
+        freq[x]++;
+    }
+
+    int mode = data[0];
+    int maxCount = 0;
+
+    // find most frequent
+    for (auto pair : freq) {
+        if (pair.second > maxCount) {
+            maxCount = pair.second;
+            mode = pair.first;
+        }
+    }
+
+    return mode, maxCount;
+}
+
+int median(std::vector<int> data){
+
+    
+    return 0;
+}
+
+
+
+
+
+void s_cal(std::string username) {
+
+    int i;
+    std::vector<int> data;
+
+    while (true) {
+        std::cout << "Input your data (type 0 to exit): ";
+
+        //!(std::cin >> i)  output t/f
+
+        if(!(std::cin >> i)) {
+            system("cls");
+            std::cout << "Invalid input! Enter a number.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        } else if (i == 0) {
+            system("cls");
+            std::cout << "Done inputing data.\nThe data is: ";
+            for(int x : data){
+                std::cout << x << " ";
+            }
+            break;
+        } else {
+        data.push_back(i);
+        system("cls");
+        }
+    }
+
+    /*
+    std::cout << "What do you want to do " << username << "?";
+    std::cout << "1. mean median modus \n2.";
+
+    char pick;
+
+    switch (pick)
+    {
+    case '1':
+    
+        break;
+    
+    default:
+        std::cout << "Invalid";
+        break;
+
+        */
+}
+    
+
+
 
 int main() {
 
-
-    int input;
-    std::cout << "Type 1 for a suprise: ";
-    std::cin >> input;
-    
-    if(input == 1)
-    {
-        //system("start https://repository.smakstlouis1sby.sch.id/13/1/Essay%20Internalisasi%20Nilai-Nilai%20Pancasila%20di%20Kelas.pdf");
-        system("start \"\" \"C:\\Users\\vallent\\Downloads\\test.jpeg\"");
-        Sleep(500);
-    }
-     else
-    {
-    std::cout << "you suck";
-    }
-
+    std::vector<int> test = {1,2,3,4,5};
+    mean(test);
     return 0;
+    
 }
